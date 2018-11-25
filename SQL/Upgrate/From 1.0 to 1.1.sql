303081066ce5fc09cfd737746673e786d329809c
Const-Phi <marina_paley@mail.ru>
2018-11-25 14:54:57 +0300

    Изменение скрипта + создание БД

--- SQL/Upgrate/From 1.0 to 1.1.sql
+++ SQL/Upgrate/From 1.0 to 1.1.sql
@@ -1,17 +1,20 @@
 CREATE TABLE Groups (
     Id     INT          PRIMARY KEY
   , [Name] NVARCHAR(25) NOT NULL
-)
+);
 
 CREATE TABLE Students (
     Id         INT           PRIMARY KEY
   , [Name]     NVARCHAR(255) NOT NULL
-  , [Id_Group] INT FOREIGN KEY REFERENCES Groups(Id)
-)
+  , [Id_Group] INT
+  , FOREIGN KEY(Id_Group) REFERENCES Groups(Id)
+);
 
-INSERT INTO Groups VALUES (1, N'ТУУ-151')
+-- Для совместимости с SQLite нужно убрать буквы N перед строковыми литералами
+
+INSERT INTO Groups VALUES (1, N'ТУУ-151');
 
 INSERT INTO Students VALUES 
 (1, N'Тимофеева Ольга',  1),
 (2, N'Козлов Александр', 2),
-(3, N'Степанова Юлия',   3)
+(3, N'Степанова Юлия',   3);
